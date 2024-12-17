84f82cbf76998e57121acb79dfdb2926dd1d1dbc
dianavlad04 <152486987+dianavlad04@users.noreply.github.com>
2024-12-17 23:31:54 +0200

    adaugare bonus+folder nume

---
+++ VladDiana/exercitiul2/ex2.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data=pd.read_csv('data.csv')
+
+data.plot()
+plt.title("Grafic 1")
+plt.xlabel("Index")
+plt.ylabel("Valoare")
+plt.legend(title="Parametrii")
+plt.savefig("grafic1.png")
+plt.show()
+
+
+data2 = data[:4]
+plt.plot(data2, marker='o')
+plt.title("Grafic 2")
+plt.xlabel("Index")
+plt.ylabel("Valoare")
+plt.legend(data2, title="Parametrii")
+plt.savefig("grafic2.png")
+plt.show()
+
+
+data3 = data[["Durata", "Puls"]][-10:]
+plt.plot(data3, marker = "*")
+plt.title("Grafic 3")
+plt.xlabel("Index")
+plt.ylabel("Valoare")
+plt.legend(data3, title="Parametrii")
+plt.savefig("grafic3.png")
+plt.show()
+
